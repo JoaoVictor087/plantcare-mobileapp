64f94b4d4015b39a6d2f366a21f13a1cf9528ac5
Joao Victor <joaovictoralves087@gmail.com>
2025-10-08 08:43:46 -0300

    Adição de um exemplo de plantas no dashboard do Usuário

--- app/(tabs)/dashboard.tsx
+++ app/(tabs)/dashboard.tsx
@@ -1,13 +1,26 @@
 import React from "react";
-import {View, Text, StyleSheet} from "react-native";
+import {View, Text, StyleSheet, ScrollView} from "react-native";
 import COLORS from "../../constants/Colors";
 
 const Dashboard = () => {
     return (
         <View style={styles.container}>
-            <Text style={styles.texto}>Dashboard</Text>
-            <View style={styles.infoArea}></View>
+            <Text style={styles.titulo}>Dashboard</Text>
+            <ScrollView contentContainerStyle={styles.scroll} showsVerticalScrollIndicator={false}>
+            <View style={styles.infoArea}>
+                <Text style={styles.texto}>Alertas:</Text>
+                <Text style={styles.texto}>Planta 1 está com baixa umidade</Text>
+                <Text style={styles.texto}>Planta 2 está com baixa temperatura</Text>
+            </View>
+            <View style={styles.infoArea}>
+                <Text style={styles.texto}>Recomendações:</Text>
+                <Text style={styles.texto}>Regue a planta 1 com mais frequência</Text>
+                <Text style={styles.texto}>Exponha a planta 2 ao sol com mais frequência</Text>
+            </View>
+            </ScrollView>
         </View>
+        
+        
     );
 };
 
@@ -17,7 +30,7 @@ const styles = StyleSheet.create({
         alignItems: "center",
         backgroundColor: COLORS.begeFundo,
     },
-    texto: {
+    titulo: {
         marginBottom: 30,
         fontSize: 30,
         fontFamily: "Inter",
@@ -28,8 +41,19 @@ const styles = StyleSheet.create({
         height: 400,
         width: 350,
         backgroundColor: "#fff",
-        borderRadius: 10
+        borderRadius: 10,
+        marginTop: 10
     },
+    texto: {
+        fontSize: 20,
+        marginLeft: 5,
+        marginTop: 10,
+        color: COLORS.verdeMedio
+    },
+    scroll: {
+    alignItems: "center",
+    paddingBottom: 20, 
+  },
 });
 
 export default Dashboard;
--- app/(tabs)/my_plants.tsx
+++ app/(tabs)/my_plants.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
 import COLORS from '../../constants/Colors';
 import { Ionicons } from '@expo/vector-icons';
 
@@ -8,7 +8,13 @@ const MyPlants = () => {
     <View style={styles.container}>
       <Text style={styles.texto}>Minhas Plantas</Text>
       <View style={styles.infoArea}>
-
+        <View style={styles.infoTexto}>
+        <Text style={styles.titulo}> Planta 1</Text>
+        <Text style={styles.titulo}> Umidade: 40%</Text>
+        <Text style={styles.titulo}> Temperatura: 24 C°</Text>
+        <Text style={styles.titulo}> Status: Saudável</Text>
+        </View>
+        <Image style={styles.image} source={require("../../assets/planta.jpg")} resizeMode="contain"></Image>
       </View>
       <TouchableOpacity>
       <Ionicons name="add-circle" size={64} style={styles.add} />
@@ -30,16 +36,32 @@ const styles = StyleSheet.create({
         color: COLORS.verdeEscuro,
         marginTop: 20,
     },
+    titulo: {
+      fontSize: 18,
+      color: COLORS.verdeMedio,
+      marginTop: 30
+      
+    },
     infoArea: {
         height: 300,
         width: 350,
         backgroundColor: "#fff",
-        borderRadius: 10
+        borderRadius: 10,
+        flexDirection: "row"
+    },
+    infoTexto: {
+        alignContent: "center",
     },
     add: {
       marginTop: 80,
       color: COLORS.verdeEscuro
-    }
+    },
+    image: {
+      height: 300,
+      width: 170,
+      borderRadius: 10,
+      marginLeft: 18,
+    },
 
     }      
   );
